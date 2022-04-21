beef8bbf70866ffad813c684e17d2d7cd532970c
Santiago Carmo <santiagocca@gmail.com>
2022-04-21 18:48:16 -0300

    removing events form bot flow

--- bot/src/crypto-bot.ts
+++ bot/src/crypto-bot.ts
@@ -6,7 +6,6 @@ import { Operation, WalletBase } from "./models/wallet-mode";
 import { ApiHelper } from "./libs/api";
 import { CandleBase } from './models/candle-base';
 import { calcRSI } from "./libs/rsi-index";
-import { State } from './libs/state';
 
 //TYPES
 import { CandleCollection, CandleType, KlineCandle, CandlePrice, CandleStartTime } from "./types/candle-types";
@@ -28,7 +27,6 @@ export class CryptoBot extends Socket {
     private _baseWSAddress:string;
     private _coins:CoinListType;
     private _candlesIntialized:boolean;
-    private _state:State;
 
     public constructor(apiAddress:string, wallet:WalletBase) {
         super();
@@ -39,7 +37,6 @@ export class CryptoBot extends Socket {
         this._currentStartTime = [];
         this._orders = [];
         this._candlesIntialized = false
-        this._state = State.getInstance();
         this._configPath = process.env.CONFIG_PATH!;
         this._sellAumont = parseFloat(process.env.SELL_AUMONT!);
         this._buyAumont = parseFloat(process.env.BUY_AUMONT!);
@@ -86,40 +83,27 @@ export class CryptoBot extends Socket {
     public onMessageHandler(event:WebSocket.MessageEvent):void {
         if(!this._candlesIntialized) return; //not start the process if the candles collection isnt initialized
 
-        const klineData = JSON.parse(event.data.toString()) as unknown as KlineCandle;  //parse the current kline data into json 
-        const ci = this._candles.findIndex(candle => candle.symbol === klineData.data.s); //get the candle collection index
-        const pi = this._prevCandlePrice.findIndex(price => price.symbol === klineData.data.s); //get the prevCandlePrice index
+        const klineData = JSON.parse(event.data.toString()) as unknown as KlineCandle;                   //parse the current kline data into json 
+        const ci = this._candles.findIndex(candle => candle.symbol === klineData.data.s);                //get the candle collection index
+        const pi = this._prevCandlePrice.findIndex(price => price.symbol === klineData.data.s);          //get the prevCandlePrice index
         const ti = this._currentStartTime.findIndex(startTime => startTime.symbol === klineData.data.s); //get the currentStartTime index
         
-        console.log(this._candles[ci].symbol, klineData.data.s, klineData.data.k.c);
+        console.log(this._candles[ci].symbol, klineData.data.s, parseFloat(klineData.data.k.c).toFixed(2));
 
         if(ci > -1 && this._candles[ci].candles.length < 1) return; // stop here if no have candles
 
         this._prevCandlePrice[pi].price = this._candles[ci].candles[this._candles[ci].candles.length -1].closePrice;   //Store the latest candle close price
         const candle = new CandleBase(klineData, this._prevCandlePrice[pi].price);                                     //convert into a Candle object
-
-        const eventkey:string = `${candle.openTimeMS}${candle.symbol}`;
-        this._state.dispatchEvent(eventkey);
     
         //PROCESS ONLY IF CURRENT CANDLE START TIME IS DIFFERENT OF THE CANDLE START TIME
         if(this._currentStartTime[ti].timestamp == candle.openTimeMS) return;
 
         if(this._candles[ci].candles.length > 15) this._candles[ci].candles.shift(); // if has more than 15 candles remove one
-        this._candles[ci].candles.push(candle);                          // push the current candle to candles array
-        this._currentStartTime[ti].timestamp = candle.openTimeMS;        // update the current start time
+        this._candles[ci].candles.push(candle);                                      // push the current candle to candles array
+        this._currentStartTime[ti].timestamp = candle.openTimeMS;                    // update the current start time
 
         //BUY AND SELL ONLY IF THE NUMBER OF CANDLES IS BIGGER THAN 13
         if(this._candles[ci].candles.length > 13) {
-            this._state.addEventListener(eventkey, {
-                type:OrderSide.BUY,
-                symbol:candle.symbol!,
-                handler: (price:number) => { 
-                    console.log(`EVENT: ${eventkey} SIZE: ${this._state.stateSize}`);
-                    console.log(`EVENT: ${eventkey} PRICE: ${price} CURRENT-PRICE: ${candle.closePrice}`);                    
-                }
-            });
-
-
             const rsi:number = calcRSI(this._candles[ci].candles.map((candle:CandleBase) => candle.closePrice)); //calculates the RSI 
             console.log(`${this._candles[ci].symbol}: ${rsi}`); //print the current RSI
 
@@ -137,9 +121,7 @@ export class CryptoBot extends Socket {
 
     private _createOrder(candle:CandleBase, orderSide:OrderSide, aumont:number):void {
         let operation = orderSide == OrderSide.BUY ? 'Comprando' : 'Vendendo';
-        console.log(`${operation} ${aumont} BTC por ${candle.closePrice} USDT`);
-
-
+        console.log(`${operation} ${aumont} ${candle.symbol} por ${candle.closePrice} USDT`);
 
         // ApiHelper.getPrivateInstance().newOrder('BTCUSDT', orderSide, OrderType.MARKET, aumont).then(response => {
         //     this._orders.push(response); //update order
