9958b68d10bee9baf1a0ae9f938f02894f0e9a2c
Santiago Carmo <santiagocca@gmail.com>
2022-10-20 19:01:21 -0300

    refactor

--- bot/src/libs/calculations/MMECalculation.ts
+++ bot/src/libs/calculations/MMECalculation.ts
@@ -23,15 +23,29 @@ export class MMECalculation implements ICalculation {
         this._mmeValues = [(this.lastMME || this.values[this._range])];
 
         this.values.slice(this._range).forEach((value, _) => {
-            this.lastMME = (value * this.exponentialWeight) + (this.lastMME * (1 - this.exponentialWeight));
+            this.lastMME = this._calcExponentialAverage(value);
             this._mmeValues.push(this.lastMME);
         });
 
         return this.currentMME;
     }
 
+    public update(newValue:number): number {
+        this.values.shift();
+        this.values.push(newValue);
+
+        this.lastMME = this._calcExponentialAverage(newValue);
+        this._mmeValues.shift();
+        this._mmeValues.push(this.lastMME);
+        return this.currentMME;
+    }
+
     private _calcExponentialWeight():number {
         return 2 / (this._range + 1);
+    }
+
+    private _calcExponentialAverage(value:number): number {
+        return (value * this.exponentialWeight) + (this.lastMME * (1 - this.exponentialWeight))
     }
     
 }
