4ad77ce5df15816d230cbbac31fb18771aa0feb7
Santiago Carmo <santiagocca@gmail.com>
2022-11-05 20:05:05 -0300

    updating the symbols candle list

--- bot/src/models/Symbol.ts
+++ bot/src/models/Symbol.ts
@@ -1,11 +1,13 @@
 import { ApiHelper } from "../api/api";
 import { CalculationFacade } from "../libs/calculations/CalculationFacade";
 import { Candle } from "./Candle";
+import { IKline, ISocketKline } from "./iKline";
 
 export class Symbol {
     private _candles: Candle[];
     private _mms: number;
     private _mme: number;
+    private _defaultKlineLimit: number;
 
     public static build = async (symbol:string, volume:number, priceChangePercent:number): Promise<Symbol> => {
         const newSymbol = new Symbol(symbol, volume, priceChangePercent);
@@ -25,6 +27,10 @@ export class Symbol {
         return this._mme;
     }
 
+    public get openTime(): number {
+        return this._candles[this.candlesSize - 1].openTime;
+    }
+
     public calculateMMS(range:number): number {
         const values = this._candles.map(candle => candle.closePrice);
         this._mms = CalculationFacade.mms(values, range).calc();
@@ -39,13 +45,21 @@ export class Symbol {
         return this.mme;
     }
 
+    public updateCandles(kline:ISocketKline|IKline) {
+        const candle = new Candle(kline, this.symbol);
+        if(this._candles.length === this._defaultKlineLimit) this._candles.shift();
+        this._candles.push(candle);
+    }
+
     private constructor(
         public symbol:string, 
         public volume:number, 
-        public priceChangePercent:number) {}
+        public priceChangePercent:number) {
+            this._defaultKlineLimit = 25;
+    }
 
     private async _getKlines() {
-        const klines = await ApiHelper.getInstance().getLatestKlines(this.symbol, '1m', 25);
+        const klines = await ApiHelper.getInstance().getLatestKlines(this.symbol, '1m', this._defaultKlineLimit);
         this._candles = klines.map(kline => new Candle(kline, this.symbol));
     }
 
