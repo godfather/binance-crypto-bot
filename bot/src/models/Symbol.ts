c446dded8aabc3f185ddfa8386835561748c72ec
Santiago Carmo <santiagocca@gmail.com>
2022-11-05 18:57:00 -0300

    calculating mms and mme from a symbol

--- bot/src/models/Symbol.ts
+++ bot/src/models/Symbol.ts
@@ -1,8 +1,11 @@
 import { ApiHelper } from "../api/api";
+import { CalculationFacade } from "../libs/calculations/CalculationFacade";
 import { Candle } from "./Candle";
 
 export class Symbol {
     private _candles: Candle[];
+    private _mms: number;
+    private _mme: number;
 
     public static build = async (symbol:string, volume:number, priceChangePercent:number): Promise<Symbol> => {
         const newSymbol = new Symbol(symbol, volume, priceChangePercent);
@@ -14,6 +17,28 @@ export class Symbol {
         return this._candles.length;
     }
 
+    public get mms(): number {
+        return this._mms;
+    }
+
+    public get mme(): number {
+        return this._mme;
+    }
+
+    public calculateMMS(range:number): number {
+        const values = this._candles.map(candle => candle.closePrice);
+        this._mms = CalculationFacade.mms(values, range).calc();
+        console.log(this.mms);
+        return this.mms;
+    }
+
+    public calculateMME(range:number): number {
+        const values = this._candles.map(candle => candle.closePrice);
+        this._mme = CalculationFacade.mme(values, range).calc();
+        console.log(this.mme);
+        return this.mme;
+    }
+
     private constructor(
         public symbol:string, 
         public volume:number, 
@@ -23,4 +48,5 @@ export class Symbol {
         const klines = await ApiHelper.getInstance().getLatestKlines(this.symbol, '1m', 25);
         this._candles = klines.map(kline => new Candle(kline, this.symbol));
     }
+
 }
--- bot/src/test/symbol.test.ts
+++ bot/src/test/symbol.test.ts
@@ -1,11 +1,25 @@
-import { expect, describe, test } from '@jest/globals';
+import { expect, describe, test, jest } from '@jest/globals';
 
 import { Symbol } from '../models/Symbol';
+
+jest.setTimeout(10000);
 
 describe('Testing the symbol class\' methods', () => {
 
     test('testing symbol creation', async () => {
         const symbol = await Symbol.build('BNBUSDT', 300, 1.2);
-        expect(symbol.klinesSize).toEqual(25);
+        expect(symbol.candlesSize).toEqual(25);
+    });
+
+    test('testing symbol mms calc', async () => {
+        const symbol = await Symbol.build('BNBUSDT', 300, 1.2);
+        const mms = symbol.calculateMMS(7);
+        expect(mms > 0).toEqual(true);        
+    });
+
+    test('testing symbol mme calc', async () => {
+        const symbol = await Symbol.build('BNBUSDT', 300, 1.2);
+        const mme = symbol.calculateMME(7);
+        expect(mme > 0).toEqual(true);        
     });
 });
