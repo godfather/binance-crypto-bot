27870e051c44705d329af853c169b402efe70367
Santiago Carmo <santiagocca@gmail.com>
2022-11-15 18:08:47 -0300

    simplifying the order class

--- bot/src/libs/orders/OrderBase.ts
+++ bot/src/libs/orders/OrderBase.ts
@@ -6,17 +6,8 @@ import { IOrder } from "../../models/Order";
 export abstract class OrderBase {
     constructor(
         public symbol: string,
-        public walletBalance: number,
         public investiment: number
     ) {}
-
-    get walletHasBalance(): boolean {
-        return this.walletBalance >= this.investiment; 
-    }
-
-    protected updateWallet(): Promise<IWallet> {
-        return ApiHelper.getPrivateInstance().getWalletInfo();
-    }
     
     abstract newOrder(quantity: number):Promise<OrderResponse>;
     abstract persistOrder(orderResponse: OrderResponse):Promise<IOrder>;
--- bot/src/test/order.test.ts
+++ bot/src/test/order.test.ts
@@ -7,13 +7,13 @@ afterAll(done => done())
 
 describe('Testing order classes', () => {
     test('Testing buy order', async () => {
-        const order = new BuyOrder('BNBUSDT', 11682.18620000, 10.00000000);
+        const order = new BuyOrder('BNBUSDT', 10.00000000);
         const result = await order.newOrder();
         expect(typeof result.orderId === 'number').toEqual(true);
     });
 
     test('Testting order persistence', async () => {
-        const order = new BuyOrder('BNBUSDT', 11682.18620000, 10.00000000);
+        const order = new BuyOrder('BNBUSDT', 10.00000000);
         const orderResponse = await order.newOrder();
         const result = await order.persistOrder(orderResponse);
         expect(result.sold!).toEqual(false);
