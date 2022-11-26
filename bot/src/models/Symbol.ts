dc68c0742ed13d25637c6245575d8ccda10e8e0b
Santiago Carmo <santiagocca@gmail.com>
2022-11-26 01:51:58 -0300

    refactor symbol's candle collection to use observable pattern

--- bot/src/models/Symbol.ts
+++ bot/src/models/Symbol.ts
@@ -1,13 +1,17 @@
 import { ApiHelper } from "../api/api";
 import { CalculationFacade } from "../libs/calculations/CalculationFacade";
+import { Observable } from "../libs/observer/Observable";
 import { Candle } from "./Candle";
 import { IKline, ISocketKline } from "./iKline";
 
 export class Symbol {
-    private _candles: Candle[];
+    private _candles: Observable<Candle[]> = new Observable<Candle[]>();
     private _mms: number;
     private _mme: number;
     private _defaultKlineLimit: number;
+
+    //observable variables
+    private _updateMetrics: Observable<boolean> = new Observable<boolean>();
 
     public static build = async (symbol:string, volume:number, priceChangePercent:number): Promise<Symbol> => {
         const newSymbol = new Symbol(symbol, volume, priceChangePercent);
@@ -15,8 +19,12 @@ export class Symbol {
         return newSymbol;
     }
 
+    public get candles(): Candle[] {
+        return this._candles.value;
+    }
+
     public get candlesSize(): number {
-        return this._candles.length;
+        return this._candles.value.length;
     }
 
     public get mms(): number {
@@ -28,27 +36,28 @@ export class Symbol {
     }
 
     public get openTime(): number {
-        return this._candles[this.candlesSize - 1].openTime;
+        return this._candles.value[this.candlesSize - 1].openTime;
     }
 
     public calculateMMS(range:number): number {
-        const values = this._candles.map(candle => candle.closePrice);
+        const values = this._candles.value.map(candle => candle.closePrice);
         this._mms = CalculationFacade.mms(values, range).calc();
-        console.log(this.mms);
+        // console.log(this.mms);
         return this.mms;
     }
 
     public calculateMME(range:number): number {
-        const values = this._candles.map(candle => candle.closePrice);
+        const values = this._candles.value.map(candle => candle.closePrice);
         this._mme = CalculationFacade.mme(values, range).calc();
-        console.log(this.mme);
+        // console.log(this.mme);
         return this.mme;
     }
 
     public updateCandles(kline:ISocketKline|IKline) {
         const candle = new Candle(kline, this.symbol);
-        if(this._candles.length === this._defaultKlineLimit) this._candles.shift();
-        this._candles.push(candle);
+        if(this._candles.value.length === this._defaultKlineLimit) this._candles.value.shift();
+        this._candles.value.push(candle);
+        this._candles.value = this._candles.value;
     }
 
     private constructor(
@@ -56,11 +65,20 @@ export class Symbol {
         public volume:number, 
         public priceChangePercent:number) {
             this._defaultKlineLimit = 25;
+            this._updateMetrics.value = false;
+            this._observers();
     }
 
     private async _getKlines() {
         const klines = await ApiHelper.getInstance().getLatestKlines(this.symbol, '1m', this._defaultKlineLimit);
-        this._candles = klines.map(kline => new Candle(kline, this.symbol));
+        this._candles.value = klines.map(kline => new Candle(kline, this.symbol));
+    }
+
+    private _observers() {
+        this._candles.subscribe(_ => {
+            console.log('candles changed');
+            // console.table(candles);
+        });
     }
 
 }
--- bot/src/test/symbol.test.ts
+++ bot/src/test/symbol.test.ts
@@ -1,4 +1,5 @@
 import { expect, describe, test, jest } from '@jest/globals';
+import { ISocketKline } from '../models/iKline';
 
 import { Symbol } from '../models/Symbol';
 
@@ -21,5 +22,42 @@ describe('Testing the symbol class\' methods', () => {
         const symbol = await Symbol.build('BNBUSDT', 300, 1.2);
         const mme = symbol.calculateMME(7);
         expect(mme > 0).toEqual(true);        
+    });
+
+    test('testing updating symbols candles', async () => {
+        const testKline:ISocketKline = {
+            stream:'test',
+            data: {
+                "e": "kline",     // Event type
+                "E": 123456789,   // Event time
+                "s": "BNBUSDT",    // Symbol
+                "k": {
+                    "t": 123400000, // Kline start time
+                    "T": 123460000, // Kline close time
+                    "s": "BNBBTC",  // Symbol
+                    "i": "1m",      // Interval
+                    "f": 100,       // First trade ID
+                    "L": 200,       // Last trade ID
+                    "o": "0.0010",  // Open price
+                    "c": "0.0020",  // Close price
+                    "h": "0.0025",  // High price
+                    "l": "0.0015",  // Low price
+                    "v": "1000",    // Base asset volume
+                    "n": 100,       // Number of trades
+                    "x": false,     // Is this kline closed?
+                    "q": "1.0000",  // Quote asset volume
+                    "V": "500",     // Taker buy base asset volume
+                    "Q": "0.500",   // Taker buy quote asset volume
+                    "B": "123456"   // Ignore
+                }
+            }
+        };
+
+        expect.assertions(2);
+
+        const symbol = await Symbol.build('BNBUSDT', 300, 1.2);
+        symbol.updateCandles(testKline);
+        expect(symbol.candles[symbol.candles.length - 1].closePrice).toEqual(0.0020);
+        expect(symbol.candlesSize).toEqual(25);
     });
 });
