e7fec36b7167d16b7e8ab2dd9a4d95507a854303
Santiago Carmo <santiagocca@gmail.com>
2023-02-14 11:52:00 -0300

    trying to simplify sell order

--- bot/src/libs/orders/SellOrder.ts
+++ bot/src/libs/orders/SellOrder.ts
@@ -13,40 +13,47 @@ export class SellOrder extends OrderBase {
     }
 
     public newOrder(): Promise<IOrder | void> {
-        return Order.find({ symbol:this.symbol, sold:false, side:OrderSide.BUY })
-            .then(orders => {
-                let volumeToSell = 0;
-                const ordersToSell: IOrder[] = [];
+        return ApiHelper.getPrivateInstance()
+                    .newOrder(this.symbol, OrderSide.SELL, OrderType.MARKET, this.minNotional)
+                    .catch(console.log)
+                    .then(orderResponse => this.persistOrder(orderResponse!));
+    }
 
-                orders.forEach(order => {
-                    const currentPaymentForQtd = (order.executedQty as number) * this._currentPrice;
+    // public newOrder(): Promise<IOrder | void> {
+    //     return Order.find({ symbol:this.symbol, sold:false, side:OrderSide.BUY })
+    //         .then(orders => {
+    //             let volumeToSell = 0;
+    //             const ordersToSell: IOrder[] = [];
+
+    //             orders.forEach(order => {
+    //                 const currentPaymentForQtd = (order.executedQty as number) * this._currentPrice;
 
                     
-                    if(currentPaymentForQtd < order.cummulativeQuoteQty) return;                    
-                    volumeToSell += order.executedQty as number;
-                    //apague
-                    console.log(`ID: ${order._id} – currentPaymentForQtd  ${currentPaymentForQtd} – cummulativeQuoteQty  ${order.cummulativeQuoteQty}`);
-                    ordersToSell.push(order);
-                });
+    //                 if(currentPaymentForQtd < order.cummulativeQuoteQty) return;                    
+    //                 volumeToSell += order.executedQty as number;
+    //                 //apague
+    //                 console.log(`ID: ${order._id} – currentPaymentForQtd  ${currentPaymentForQtd} – cummulativeQuoteQty  ${order.cummulativeQuoteQty}`);
+    //                 ordersToSell.push(order);
+    //             });
                 
-                const currentVolumePriceToSell = volumeToSell * this._currentPrice;
-                if(currentVolumePriceToSell < this.minNotional) return;
+    //             const currentVolumePriceToSell = volumeToSell * this._currentPrice;
+    //             if(currentVolumePriceToSell < this.minNotional) return;
 
-                return ApiHelper.getPrivateInstance()
-                    .newOrder(this.symbol, OrderSide.SELL, OrderType.MARKET, volumeToSell)
-                    .catch(console.log)
-                    .then(orderResponse => {
-                        if(!orderResponse) return;
-                        Promise.all(ordersToSell.map(order => {
-                            order.sold = true;
-                            order.save();
-                        }));
+    //             return ApiHelper.getPrivateInstance()
+    //                 .newOrder(this.symbol, OrderSide.SELL, OrderType.MARKET, volumeToSell)
+    //                 .catch(console.log)
+    //                 .then(orderResponse => {
+    //                     if(!orderResponse) return;
+    //                     Promise.all(ordersToSell.map(order => {
+    //                         order.sold = true;
+    //                         order.save();
+    //                     }));
 
-                        orderResponse.sold = true;
-                        return this.persistOrder(orderResponse);
-                    });
-            }).catch(error => console.log(error.message));
-    }
+    //                     orderResponse.sold = true;
+    //                     return this.persistOrder(orderResponse);
+    //                 });
+    //         }).catch(error => console.log(error.message));
+    // }
 
     public persistOrder(orderResponse: OrderResponse): Promise<IOrder> {
         return new Order(orderResponse).save();
